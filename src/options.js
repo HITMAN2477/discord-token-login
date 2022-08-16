b8d3ec3b61c8de1843ec48d839a9dd221b3b6287
umgustavo <gugu.r.azevedo@gmail.com>
2022-08-16 15:25:08 -0300

    Remove token validation & add update check

--- src/options.js
+++ src/options.js
@@ -1,5 +1,52 @@
 const saveBtn = document.getElementById('save-button')
+const closeAlertBtn = document.getElementById('close-alert-button')
 const tokenInput = document.getElementById('token-input')
+const alertEl = document.getElementById('alert')
+
+function showAlert() {
+    alertEl.classList.remove('hidden')
+}
+
+function hideAlert() {
+    alertEl.classList.add('hidden')
+}
+
+function setAlertHTML(html) {
+    alertEl.children[0].innerHTML = html
+}
+
+async function checkUpdates() {
+    const version = chrome.runtime.getManifest()?.version
+    if (!version) return
+
+    function formatVersion(ver) {
+        return parseInt(ver.replaceAll('.', '').replaceAll('v', ''))
+    }
+
+    try {
+        const res = await fetch(
+            'https://api.github.com/repos/umgustavo/discord-token-login/releases/latest'
+        )
+        const data = await res.json()
+        const lastVersion = formatVersion(data.name)
+        const downloadUrl = data.assets[0].browser_download_url
+        let ver = formatVersion(version)
+
+        console.log('Version checked successfully.')
+        console.log('Current version: v' + version)
+        console.log('Last version: ' + data.name)
+
+        if (ver < lastVersion) {
+            setAlertHTML(
+                `New version available! <a href="${downloadUrl}" target="_blank">Click here</a> to download the <b>${data.name}</b>. (current: <b>v${version}</b>)`
+            )
+            showAlert()
+        }
+    } catch (err) {
+        console.error('An error occurred while checking for updates:')
+        console.error(err)
+    }
+}
 
 function getCurrentToken() {
     chrome.storage.sync.get('token', function (res) {
@@ -9,14 +56,11 @@ function getCurrentToken() {
     })
 }
 
+checkUpdates()
 getCurrentToken()
 
 function isTokenValid(token) {
-    const regex = /[MN][A-Za-z\d]{23}\.[\w-]{6}\.[\w-]{27}/g
-    if (token.match(regex)) {
-        saveBtn.disabled = false
-        return true
-    }
+    if (token.length !== 0) return true
     return false
 }
 
@@ -30,7 +74,12 @@ saveBtn.addEventListener('click', () => {
     const token = tokenInput.value
     if (isTokenValid(token)) {
         chrome.storage.sync.set({ token: token }, () => {
-            alert('Token saved!')
+            setAlertHTML('Token saved successfully!')
+            showAlert()
         })
     }
+})
+
+closeAlertBtn.addEventListener('click', () => {
+    hideAlert()
 })
